43d1342ba3874089eb8e72578485825bd70ecec6
Christian De Martino <chrisddm@gmail.com>
2018-09-24 19:51:20 -0300

    test

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: path.join(__dirname, 'src', 'index.js'),
+  entry: path.join(__dirname, "/src/index.jsx"),
   devtool: 'source-map',
   module: {
     rules: [
